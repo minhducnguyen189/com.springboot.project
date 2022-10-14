df3f7ea3fe7a44a54bc9a26c91e3a507d94851c2
DucNguyen <minhducnguyen189@gmail.com>
2022-10-14 20:01:23 +0700

    update example for openapi config pom.xml

---
+++ openapi-advance-app-config-pom/src/main/java/com/springboot/pom/OpenApiAdvanceAppConfig.java
@@ -0,0 +1,11 @@
+package com.springboot.pom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiAdvanceAppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenApiAdvanceAppConfig.class, args);
+    }
+}
