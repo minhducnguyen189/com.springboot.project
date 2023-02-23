840f4d1a3515e8f679e136edc2a8a0815425a1f9
ducnguyen <minhducnguyen189@gmail.com>
2023-02-23 21:35:17 +0700

    format code

--- apache-cxf/src/main/java/com/springboot/apache/cxf/config/CountryInfoProperties.java
+++ apache-cxf/src/main/java/com/springboot/apache/cxf/config/CountryInfoProperties.java
@@ -1,23 +1,19 @@
 package com.springboot.apache.cxf.config;
 
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Getter
-@Configuration
+@Setter
+@Component
+@ConfigurationProperties(prefix = "country.client.soap")
 public class CountryInfoProperties {
 
-    @Value("${country.client.soap.hostname}")
     private String hostname;
-
-    @Value("${country.client.soap.protocol}")
     private String protocol;
-
-    @Value("${country.client.soap.port}")
     private String port;
-
-    @Value("${country.client.soap.api}")
     private String api;
 
 }
