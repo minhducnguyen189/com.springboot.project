4eb958e2ebc2d19d5804bf8b7d10fec1b71dcec2
Duc Nguyen <minhducnguyen189@gmail.com>
2023-08-04 21:15:17 +0700

    update example for update content type

---
+++ config-content-type-response-app/src/main/java/com/springboot/project/config/content/type/response/app/model/CustomerRequest.java
@@ -0,0 +1,61 @@
+package com.springboot.project.com.exception.handler.app.model;
+
+import java.util.Date;
+
+public class CustomerRequest {
+
+    private String fullName;
+    private String email;
+    private String address;
+    private String phone;
+    private Gender gender;
+    private Date dob;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+}
