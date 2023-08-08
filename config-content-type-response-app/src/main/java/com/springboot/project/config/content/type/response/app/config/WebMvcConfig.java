b843bc83628a82b7656be6c20ef1bc0128c80133
Duc Nguyen <minhducnguyen189@gmail.com>
2023-08-08 21:09:02 +0700

    update

--- config-content-type-response-app/src/main/java/com/springboot/project/config/content/type/response/app/config/WebMvcConfig.java
+++ config-content-type-response-app/src/main/java/com/springboot/project/config/content/type/response/app/config/WebMvcConfig.java
@@ -1,28 +1,16 @@
 package com.springboot.project.config.content.type.response.app.config;
 
-import com.springboot.project.config.content.type.response.app.intercepter.CustomerResponseInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private CustomerResponseInterceptor customerResponseInterceptor;
-
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON);
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(this.customerResponseInterceptor)
-                .addPathPatterns("/v1/**");
     }
 
 }
